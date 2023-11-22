0e8af8adc85befdd12ebe55375a918344be4bbb8
Andrew Thomas <athomas94@gmail.com>
2023-11-21 20:13:00 -0500

    adding completed JS for the game

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 class Casino {
-    // Write code here
+    constructor(name) {
+        this.name = name
+        this.timesPlayed = 0
+        this.totalWinnings = 0
+    }
+
+    playGame(bet) {
+        this.timesPlayed++
+
+        if (Math.random() <= 0.5) { 
+            console.log("The " + this.name + " wins " + bet +" monies!") 
+            console.log("You have now played at the " + this.name + " " + this.timesPlayed + " time(s)") 
+            this.totalWinnings = this.totalWinnings - bet
+        }
+        else {
+            console.log("The player wins " + bet +" monies!") 
+            this.totalWinnings = this.totalWinnings + bet
+            console.log("You have now played at the " + this.name + " " + this.timesPlayed + " time(s) and won a total of $" + this.totalWinnings + "!")
+        }
+    }
 };
 
 // TESTS
 const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
-
+console.log(myCasino);
 // BONUS TESTS
 /*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
